9e91f0a382ebed03aad097e1b4175d7edb72175a
n.kremnev <n.kremnev@bnovo.ru>
2026-01-03 17:02:12 +0300

    add search needle in directory

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -12,7 +14,8 @@ func searchNeedleInFile(path, needle string) {
 	f, err := os.Open(path)
 
 	if err != nil {
-		log.Fatal("Ошибка при чтении файла")
+		log.Printf("Ошибка при чтении файла %s: %v", path, err)
+		return
 	}
 
 	defer f.Close()
@@ -23,10 +26,35 @@ func searchNeedleInFile(path, needle string) {
 		line := scanner.Text()
 
 		if strings.Contains(line, needle) {
+			fmt.Printf("Файл: %s\n", path)
 			fmt.Printf("%v:%v:%v\n", path, lineNum, line)
 		}
 	}
 
+	if err := scanner.Err(); err != nil {
+		log.Printf("Ошибка при сканировании файла %s: %v", path, err)
+	}
+}
+
+func searchNeedleInDirectory(path, needle string) {
+	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Printf("предотвращение ошибки доступа к пути %s: %v\n", path, err)
+			return nil
+		}
+
+		if d.IsDir() {
+			fmt.Printf("Директория: %s\n", path)
+			return nil
+		} else {
+			searchNeedleInFile(path, needle)
+			return nil
+		}
+	})
+
+	if err != nil {
+		log.Fatalf("Ошибка при обходе директории: %v", err)
+	}
 }
 
 func main() {
@@ -41,11 +69,13 @@ func main() {
 	file, err := os.Stat(path)
 
 	if err != nil {
-		log.Fatal("Файл или папка не найдены")
+		log.Fatalf("Ошибка: %v", err)
 	}
 
 	if !file.IsDir() {
 		searchNeedleInFile(path, needle)
+	} else {
+		searchNeedleInDirectory(path, needle)
 	}
 
 }
